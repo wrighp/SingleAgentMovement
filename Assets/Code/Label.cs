8de339a9e32c279198bea7cfc35b790efa45d7ea
samsuite <suitester@gmail.com>
2017-09-24 19:25:20 -0400

    labels for agents

---
+++ Assets/Code/Label.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Label : MonoBehaviour {
+
+    public Transform following;
+
+	void Update () {
+		transform.position = following.transform.position + Vector3.up;
+	}
+}
